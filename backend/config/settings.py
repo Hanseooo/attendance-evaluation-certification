a983c476b9c2758dfeb8a7d7b3224263b0ec6ae5
Hanseooo <amoguishans@gmail.com>
2025-11-27 16:22:35 +0800

    assigned a $PORT

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -209,13 +209,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://hcdc-podium.vercel.app",
-#     "http://localhost:5173",
-#     "http://127.0.0.1:5173",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://hcdc-podium.vercel.app",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_HEADERS = [
     "accept",
