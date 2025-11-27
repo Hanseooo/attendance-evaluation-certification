ea0874157bc053af58ac12feebd287a8819227b5
Hanseooo <amoguishans@gmail.com>
2025-11-27 14:28:28 +0800

    added the missing comma

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -88,7 +88,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -155,7 +155,6 @@ DATABASES = {
         )
     }
 
-import dj_database_url
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
